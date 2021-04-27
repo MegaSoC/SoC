7641bd3262fbfa870f6225cb18213713d1216bc3
Yunhao Tian <t123yh@outlook.com>
2021-04-27 20:45:22 +0800

    Add synchronizers for ethernet

--- ethernet_wrapper.sv
+++ ethernet_wrapper.sv
@@ -32,8 +32,21 @@ wire   [3:0]   mrxd_0;
 wire           mrxdv_0;
 wire           mrxerr_0;
 
-wire           mcoll_0;
-wire           mcrs_0;
+wire           mcoll_soc, mcoll_ref;
+wire           mcrs_soc, mcrs_ref;
+
+stolen_cdc_array_single #(2, 1, 2) crs_cdc(
+   .src_clk(rmii_ref_clk),
+   .src_in({mcoll_ref, mcrs_ref}),
+   .dest_clk(aclk),
+   .dest_out({mcoll_soc, mcrs_soc})
+);
+
+stolen_cdc_sync_rst cpu_rstgen(
+    .dest_clk(rmii_ref_clk),
+    .dest_rst(phy_rstn),
+    .src_rst(aresetn)
+);
 
 axi_ethernetlite #(
   .C_S_AXI_ACLK_PERIOD_PS(7500),
@@ -79,12 +92,11 @@ axi_ethernetlite #(
     
   .phy_tx_clk(mtxclk_0),
   .phy_rx_clk(mrxclk_0),
-  .phy_crs(mcrs_0),
+  .phy_crs(mcrs_soc),
   .phy_dv(mrxdv_0),
   .phy_rx_data(mrxd_0),
-  .phy_col(mcoll_0),
+  .phy_col(mcoll_soc),
   .phy_rx_er(mrxerr_0),
-  .phy_rst_n(phy_rstn),
   .phy_tx_en(mtxen_0),
   .phy_tx_data(mtxd_0),
   .phy_mdio_i(md_i_0),
@@ -104,8 +116,8 @@ mii_to_rmii #(
     .mac2rmii_tx_er(1'b0),
     .rmii2mac_tx_clk(mtxclk_0),
     .rmii2mac_rx_clk(mrxclk_0),
-    .rmii2mac_col(mcoll_0),
-    .rmii2mac_crs(mcrs_0),
+    .rmii2mac_col(mcoll_ref),
+    .rmii2mac_crs(mcrs_ref),
     .rmii2mac_rx_dv(mrxdv_0),
     .rmii2mac_rx_er(mrxerr_0),
     .rmii2mac_rxd(mrxd_0),
